1ead469eb705dd9535a21f0c3429a92f583c8008
nico-stan <108010607+nico-stan@users.noreply.github.com>
2022-07-31 03:02:09 +0200

    Add files via upload

--- src/clean.py
+++ src/clean.py
@@ -37,11 +37,10 @@ def get_results_from_foursquare (key, query, location, radius, limit):
     if response:
         try:
             results=response.json()['results']
-            new_list = [ {"name" : query_ok,
+            new_list = [ {"name" : query.strip(),
                       "lat"  : i["geocodes"]["main"]["latitude"],
-                      "long" : i["geocodes"]["main"]["longitude"],
-                      "type" : {"typepoint":
-                                  {"type": "Point"}}} for i in results]
+                      "long" : i["geocodes"]["main"]["longitude"] 
+                      "type" : {"typepoint": {"type": "Point"}} } for i in results]
 
             df = pd.DataFrame(new_list)
             return df
